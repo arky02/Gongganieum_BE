ea585a15a66ba074398f9f0bf765fb5ed90f7b0b
arky02 <kyean07@naver.com>
2024-05-23 10:43:21 +0900

    fix: fix port num error

--- config/maria.js
+++ config/maria.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 
 var db_info = {
   host: process.env.DB_HOST,
-  port: 3003,
+  port: 3306,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: "ruslzjaxj",
   database: process.env.DB_NAME,
   charset: "utf8mb4",
 };
