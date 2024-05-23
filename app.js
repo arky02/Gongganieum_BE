49b6079d0bf7188e1d4b6581727d7faf9d2243d5
arky02 <kyean07@naver.com>
2024-05-23 16:33:29 +0900

    fix: fix test

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var apiRouter = require("./api");
 var createError = require("http-errors");
 
 // DB
-// var maria = require("./config/maria");
-// maria.connect();
+var maria = require("./config/maria");
+maria.connect();
 
 // CORS Setup
 var allowlist = ["http://localhost:3000"];
@@ -41,30 +41,30 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-app.set("port", process.env.PORT || 8080);
-app.set("host", process.env.HOST || "0.0.0.0");
+// app.set("port", process.env.PORT || 8080);
+// app.set("host", process.env.HOST || "0.0.0.0");
 
-app.use(
-  cors({
-    origin(req, res) {
-      console.log("접속된 주소: " + req),
-        -1 == allowlist.indexOf(req) && req
-          ? res(Error("허가되지 않은 주소입니다."))
-          : res(null, !0);
-    },
-    credentials: !0,
-    optionsSuccessStatus: 200,
-  })
-);
+// app.use(
+//   cors({
+//     origin(req, res) {
+//       console.log("접속된 주소: " + req),
+//         -1 == allowlist.indexOf(req) && req
+//           ? res(Error("허가되지 않은 주소입니다."))
+//           : res(null, !0);
+//     },
+//     credentials: !0,
+//     optionsSuccessStatus: 200,
+//   })
+// );
 
-// app.get("/", function (req, res) {
-//   res.send("접속된 아이피: " + req.ip);
-// });
+// // app.get("/", function (req, res) {
+// //   res.send("접속된 아이피: " + req.ip);
+// // });
 
-app.listen(app.get("port"), app.get("host"), () =>
-  console.log(
-    "Server is running on : " + app.get("host") + ":" + app.get("port")
-  )
-);
+// app.listen(app.get("port"), app.get("host"), () =>
+//   console.log(
+//     "Server is running on : " + app.get("host") + ":" + app.get("port")
+//   )
+// );
 
 module.exports = app;
