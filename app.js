afbe84f1f60af4df4abe6d16683ef4aed36bc003
arky02 <kyean07@naver.com>
2024-05-27 10:22:59 +0900

    feat: allowlist url 수정

--- app.js
+++ app.js
@@ -22,8 +22,9 @@ maria.connect();
 
 // CORS Setup
 var allowlist = [
-  "http://localhost:3000",
-  "http://ec2-3-23-49-89.us-east-2.compute.amazonaws.com",
+  "http://localhost:3000/**",
+  "http://ec2-3-23-49-89.us-east-2.compute.amazonaws.com/**",
+  "http://ec2-3-23-49-89.us-east-2.compute.amazonaws.com:8080/**",
 ];
 var corsOptionsDelegate = function (req, callback) {
   var corsOptions;
