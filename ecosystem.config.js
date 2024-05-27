95f5572303620c084bccaea1cb9b492df04b089b
arky02 <kyean07@naver.com>
2024-05-27 11:12:33 +0900

    chore: modify pm2 ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,13 @@ module.exports = {
     {
       name: "poppop-backend",
       script: "app.js",
+      watch: ["server", "client"],
       instances: 1,
       exec_mode: "cluster",
       exec_interpreter: "node",
+      // Delay between restart
+      watch_delay: 1000,
+      ignore_watch: ["node_modules", "client/img", "\\.git", "*.log"],
     },
   ],
 };
