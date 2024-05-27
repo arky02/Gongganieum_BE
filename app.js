b766217afee74f723aadc01acfdc306be12e399f
arky02 <kyean07@naver.com>
2024-05-27 13:35:05 +0900

    fix: try fixing swagger cors error

--- app.js
+++ app.js
@@ -21,17 +21,45 @@ var maria = require("./config/maria");
 maria.connect();
 
 // CORS Setup
+
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", ...allowlist);
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 var allowlist = [
   "http://localhost:3000/*",
   "http://ec2-3-23-49-89.us-east-2.compute.amazonaws.com/**",
   "http://ec2-3-23-49-89.us-east-2.compute.amazonaws.com:8080/**",
 ];
-var corsOptionsDelegate = function (req, callback) {
+
+var corsOptionsDelegate = function (req, res, callback) {
   var corsOptions;
   if (allowlist.indexOf(req.header("Origin")) !== -1) {
     corsOptions = { origin: true }; // reflect (enable) the requested origin
   } else {
     corsOptions = { origin: false };
+    res(Error("허가되지 않은 주소입니다."));
   }
   callback(null, corsOptions); // error, options
 };
