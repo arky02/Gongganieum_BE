738549f9b8b639af23e9344f26fffdd907f18998
arky02 <kyean07@naver.com>
2024-06-11 10:01:39 +0900

    test: api route test 수정

--- routes/apis/popup.js
+++ routes/apis/popup.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const maria = require("../config/maria");
+const maria = require("/config/maria");
 
 // Res: Popups(_id: int, name: str, type: str, date: str, address: str, keyword: str, building: str)
 router.get("/infos", (req, res) => {
