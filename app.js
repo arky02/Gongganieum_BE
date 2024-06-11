ed46b1472188f6e31efab43265b699bf4c354bc7
arky02 <kyean07@naver.com>
2024-06-11 10:08:56 +0900

    style: 주석 삭제

--- app.js
+++ app.js
@@ -19,33 +19,6 @@ const swaggerFile = require("./swagger_output.json");
 var maria = require("./config/maria");
 maria.connect();
 
-// CORS Setup
-
-// // Add headers
-// app.use(function (req, res, next) {
-//   // Website you wish to allow to connect
-//   res.setHeader("Access-Control-Allow-Origin", ...allowlist);
-
-//   // Request methods you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
-
-//   // Request headers you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,content-type"
-//   );
-
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-
-//   // Pass to next layer of middleware
-//   next();
-// });
-
 var allowlist = [
   "http://localhost:3000",
   "http://ec2-3-23-49-89.us-east-2.compute.amazonaws.com/**",
