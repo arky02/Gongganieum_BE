1fe3f4004556c8daa3e9bb683da21d7e3b57c29e
arky02 <kyean07@naver.com>
2024-06-17 11:39:46 +0900

    chore: 바뀐 db 주소 적용

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ maria.connect();
 var allowlist = [
   "http://localhost:3000",
   "http://ec2-3-23-49-89.us-east-2.compute.amazonaws.com/**",
-  "http://ec2-3-23-49-89.us-east-2.compute.amazonaws.com:8080/**",
+  "http://ec2-3-23-49-89.us-east-2.compute.amazonaws.com/**",
+  "http://ec2-3-34-222-145.ap-northeast-2.compute.amazonaws.com:8080/**",
+  "http://ec2-3-34-222-145.ap-northeast-2.compute.amazonaws.com:8080/**",
 ];
 
 var corsOptionsDelegate = function (req, callback) {
