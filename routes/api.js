bc876e5f6d9a68116c5cfcb66c76d494275259d8
arky02 <kyean07@naver.com>
2024-06-18 14:27:11 +0900

    feat: /building/search latest_end_date

--- routes/api.js
+++ routes/api.js
@@ -102,20 +102,12 @@ router.get("/building/search", (req, res) => {
             b.isours,
             b.name,
             b.tag,
-            (
-                SELECT JSON_ARRAYAGG(popup ORDER BY STR_TO_DATE(SUBSTRING_INDEX(JSON_UNQUOTE(JSON_EXTRACT(popup.value, '$.date')), ' - ', 1), '%y.%m.%d') DESC)
-                FROM JSON_TABLE(
-                    b.popups,
-                    '$[*]'
-                    COLUMNS (
-                        value JSON PATH '$'
-                    )
-                ) AS popup
-            ) AS sorted_popups,
+            b.popups,
             MIN(STR_TO_DATE(SUBSTRING_INDEX(JSON_UNQUOTE(JSON_EXTRACT(popup.value, '$.date')), ' - ', 1), '%y.%m.%d')) AS earliest_start_date,
             MAX(STR_TO_DATE(SUBSTRING_INDEX(JSON_UNQUOTE(JSON_EXTRACT(popup.value, '$.date')), ' - ', -1), '%y.%m.%d')) AS latest_end_date
         FROM 
-            Buildings b,
+            Buildings b
+        JOIN 
             JSON_TABLE(
                 b.popups, 
                 '$[*]' 
