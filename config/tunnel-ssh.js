e70549a9ded844df46e704cafddd157d86107df6
arky02 <kyean07@naver.com>
2024-06-19 14:09:29 +0900

    fix: edit tunnel-ssh setting

--- config/tunnel-ssh.js
+++ config/tunnel-ssh.js
@@ -1,26 +1,30 @@
-import mysql from "mysql2";
-import tunnel from "tunnel-ssh";
-import dotenv from "dotenv";
-dotenv.config();
+var mysql = require("mysql2");
+var tunnel = require("tunnel-ssh");
+require("dotenv").config();
 
-const ssh_config = {
+var ssh_config = {
   username: process.env.SSH_USER,
   password: process.env.SSH_PASSWORD,
   host: process.env.SSH_HOST,
   port: process.env.SSH_PORT,
   dstHost: process.env.SSH_DATABASE_HOST,
   dstPort: process.env.SSH_DATABASE_PORT,
+  localHost: "127.0.0.1", // 로컬 호스트를 설정합니다.
+  localPort: 3306, // 로컬 포트를 설정합니다.
 };
 
 tunnel(ssh_config, (error, server) => {
   if (error) {
     throw error;
   } else if (server !== null) {
-    mysql.createConnection({
+    var conn = mysql.createConnection({
       host: process.env.DATABASE_HOST,
+      port: 3306,
       user: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
     });
   }
 });
+
+module.exports = conn;
