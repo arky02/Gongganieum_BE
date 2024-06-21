f1afbd54c7801ef43ba35235032aa5f9055d8d1d
arky02 <kyean07@naver.com>
2024-06-21 13:50:03 +0900

    style: 주석 삭제

--- app.js
+++ app.js
@@ -61,30 +61,4 @@ app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
 
-// app.set("port", process.env.PORT || 8080);
-// app.set("host", process.env.HOST || "0.0.0.0");
-
-// app.use(
-//   cors({
-//     origin(req, res) {
-//       console.log("접속된 주소: " + req),
-//         -1 == allowlist.indexOf(req) && req
-//           ? res(Error("허가되지 않은 주소입니다."))
-//           : res(null, !0);
-//     },
-//     credentials: !0,
-//     optionsSuccessStatus: 200,
-//   })
-// );
-
-// // app.get("/", function (req, res) {
-// //   res.send("접속된 아이피: " + req.ip);
-// // });
-
-// app.listen(app.get("port"), app.get("host"), () =>
-//   console.log(
-//     "Server is running on : " + app.get("host") + ":" + app.get("port")
-//   )
-// );
-
 module.exports = app;
