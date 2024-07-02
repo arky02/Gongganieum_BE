c89454ee1359aab864e066643f2baf089ee3078b
arky02 <kyean07@naver.com>
2024-07-02 11:50:32 +0900

    feat(constant.js): OAuth 로그인에 필요한 상수들 정리

---
+++ constants.js
@@ -0,0 +1,19 @@
+const OAUTH_GET_TOKEN_URL = {
+  naver: "https://kauth.kakao.com/oauth/token",
+  kakao: "https://nid.naver.com/oauth2.0/token",
+};
+
+const OAUTH_GET_USERINFO_URL = {
+  naver: "https://openapi.naver.com/v1/nid/me",
+  kakao: "https://kapi.kakao.com/v2/user/me",
+};
+
+const OAUTH_CLIENT_SECRET = {
+  naver: process.env.OAUTH_NAVER_CLIENT_SECRET,
+  kakao: process.env.OAUTH_KAKAO_CLIENT_SECRET,
+};
+
+const OAUTH_CLIENT_ID = {
+  naver: process.env.OAUTH_NAVER_CLIENT_ID,
+  kakao: process.env.OAUTH_KAKAO_CLIENT_ID,
+};
