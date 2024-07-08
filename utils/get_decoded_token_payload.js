76c9feece3cd3e7fa9257b0fff969b056801a053
arky02 <kyean07@naver.com>
2024-07-08 10:32:32 +0900

    feat: token payload decode 중 token이 undefined일 경우 예외 처리

--- utils/get_decoded_token_payload.js
+++ utils/get_decoded_token_payload.js
@@ -3,6 +3,10 @@ const { decodePayload } = require("./jwt.js");
 function getDecodedTokenPayload(req) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    console.log("No Authorization Header Token Provided!");
+    return false;
+  }
   console.log("Authorization Header Token: ", token);
 
   const payload = decodePayload(token);
