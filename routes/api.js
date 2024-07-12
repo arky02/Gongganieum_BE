ce71c0b10afdddc96655b28c98b6b5f8c53a113b
arky02 <kyean07@naver.com>
2024-07-12 11:24:11 +0900

    refactor: 관리자 페이지 API 섹션 분리

--- routes/api.js
+++ routes/api.js
@@ -27,73 +27,6 @@ const s3 = new S3Client({
   },
 });
 
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: "poppop-bucket",
-    key: function (요청, file, cb) {
-      cb(null, Date.now().toString()); //업로드시 파일명 변경가능
-    },
-  }),
-});
-
-// /admin/add/building
-// /admin/edit/building?type=(img|popup|building)&id=int
-
-router.post(
-  "/admin/edit/building",
-  upload.array("file", 20),
-  async (req, res) => {
-    const type = req.query?.type ?? null; // type = img | popup | building
-    const buildingId = req.query?.id ?? null; // 빌딩 id
-
-    if (req.files === undefined) {
-      console.log("Request에 이미지 없음 ! => req.files === undefined");
-      res.status(400).send("ERR: No Imgs Given!");
-    }
-    try {
-      const imgUrlsList = req.files.map((fileEl) => fileEl.location);
-      if (!imgUrlsList || !imgUrlsList.length > 0) {
-        console.log("ERR: imgUrlsList ERROR");
-        return;
-      }
-      console.log(
-        "AWS S3에 이미지 업로드 완료 \nAWS S3 imgUrlsList: ",
-        imgUrlsList
-      ); // imgUrlsList 존재!
-
-      maria.query(
-        `UPDATE Buildings SET img="${imgUrlsList.join(
-          ","
-        )}" WHERE _id=${buildingId};
-        SELECT * FROM Buildings WHERE _id=${buildingId};
-        `,
-        function (err, result) {
-          if (!err) {
-            console.log(
-              `빌딩 ID ${buildingId}의 건물정보 DB에 이미지 추가 성공! \nAWS S3 이미지 DB, Mysql DB에 이미지 ${imgUrlsList.length}개 추가됨`
-            );
-            res.status(200).send(result[1][0]);
-          } else {
-            console.log("ERR : " + err);
-            res.status(500).json({
-              error: "Error",
-            });
-          }
-        }
-      );
-    } catch (e) {
-      console.log(e);
-      if (e === "LIMIT_UNEXPECTED_FILE") {
-        res.status(500).send("이미지 크기가 초과되었습니다.");
-        return;
-      } else {
-        res.status(500).send("SERVER ERROR");
-      }
-    }
-  }
-);
-
 // =================================================================================================
 // Popup API : Popup 관련 API (GET) - 1개
 // =================================================================================================
@@ -714,6 +647,78 @@ router.get("/oauth/callback", async (req, res) => {
     }
   );
 });
+
+// =================================================================================================
+//  관리자 페이지 API
+// =================================================================================================
+
+// aws s3 이미지 업로드 함수
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "poppop-bucket",
+    key: function (요청, file, cb) {
+      cb(null, Date.now().toString()); //업로드시 파일명 변경가능
+    },
+  }),
+});
+
+// /admin/add/building
+// /admin/edit/building?type=(img|popup|building)&id=int
+
+router.post(
+  "/admin/edit/building",
+  upload.array("file", 20),
+  async (req, res) => {
+    const type = req.query?.type ?? null; // type = img | popup | building
+    const buildingId = req.query?.id ?? null; // 빌딩 id
+
+    if (req.files === undefined) {
+      console.log("Request에 이미지 없음 ! => req.files === undefined");
+      res.status(400).send("ERR: No Imgs Given!");
+    }
+    try {
+      const imgUrlsList = req.files.map((fileEl) => fileEl.location);
+      if (!imgUrlsList || !imgUrlsList.length > 0) {
+        console.log("ERR: imgUrlsList ERROR");
+        return;
+      }
+      console.log(
+        "AWS S3에 이미지 업로드 완료 \nAWS S3 imgUrlsList: ",
+        imgUrlsList
+      ); // imgUrlsList 존재!
+
+      maria.query(
+        `UPDATE Buildings SET img="${imgUrlsList.join(
+          ","
+        )}" WHERE _id=${buildingId};
+        SELECT * FROM Buildings WHERE _id=${buildingId};
+        `,
+        function (err, result) {
+          if (!err) {
+            console.log(
+              `빌딩 ID ${buildingId}의 건물정보 DB에 이미지 추가 성공! \nAWS S3 이미지 DB, Mysql DB에 이미지 ${imgUrlsList.length}개 추가됨`
+            );
+            res.status(200).send(result[1][0]);
+          } else {
+            console.log("ERR : " + err);
+            res.status(500).json({
+              error: "Error",
+            });
+          }
+        }
+      );
+    } catch (e) {
+      console.log(e);
+      if (e === "LIMIT_UNEXPECTED_FILE") {
+        res.status(500).send("이미지 크기가 초과되었습니다.");
+        return;
+      } else {
+        res.status(500).send("SERVER ERROR");
+      }
+    }
+  }
+);
 
 // =================================================================================================
 //  공공데이터 API
