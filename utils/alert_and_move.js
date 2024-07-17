5883677aae020f722fc4b3a56a0a304f7e42b87f
arky02 <kyean07@naver.com>
2024-07-17 16:24:34 +0900

    remove: 사용하지 않는 파일 삭제

--- utils/alert_and_move.js
+++
@@ -1,8 +0,0 @@
-function alert_and_move(path, msg) {
-  return `<script>
-            alert('${msg}')
-            location.href = '${path}'
-            </script>`;
-}
-
-module.exports = alert_and_move;
