67e0fb35cd445c7d7e2941091ea9287f564220d6
arky02 <kyean07@naver.com>
2024-07-29 17:16:28 +0900

    fix: /magazine/upload_image 중복 작성 error

--- routes/api/magazines.js
+++ routes/api/magazines.js
@@ -28,43 +28,6 @@ const uploadImgToS3 = multer({
   }),
 });
 
-router.post("/upload_image", function (req, res) {
-  /*
-  #swagger.tags = ['Magazine']
-  #swagger.summary = 'Magazine 이미지 업로드'
-  #swagger.description = 'POST Test Api 입니다.'
-*/
-
-  var name = "";
-  var age = 0;
-  try {
-    name = req.body.name;
-    age = req.body.age;
-  } catch (e) {
-    console.log("ERR (get request) : " + e);
-    res.status(400).json({
-      error: "ERR_PARAMS : email or name is not valid",
-    });
-  }
-
-  maria.query(
-    `INSERT INTO Test(name,age) VALUES ("${name}", ${age})`,
-    function (err) {
-      if (!err) {
-        console.log("(Save User) User is saved : " + name);
-        res.status(200).json({
-          message: "User is saved",
-        });
-      } else {
-        console.log("ERR (Save User) : " + err);
-        res.status(409).json({
-          error: "body 형식이 틀리거나 데이터베이스에 문제가 발생했습니다.",
-        });
-      }
-    }
-  );
-});
-
 // 매거진 이미지 업로드
 router.post("/upload_image", uploadImgToS3.single("file"), async (req, res) => {
   /*
