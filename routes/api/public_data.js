9e7b8969774c43d79e38667a8579efbbe13ef9dc
arky02 <kyean07@naver.com>
2024-07-31 17:20:18 +0900

    style: add logs to public data api

--- routes/api/public_data.js
+++ routes/api/public_data.js
@@ -9,8 +9,8 @@ const axios = require("axios");
 router.get("/address_code", async (req, res) => {
   /*
   #swagger.tags = ['공공데이터']
-  #swagger.summary = '공공데이터 - 주소 코드'
-  #swagger.description = ""
+  #swagger.summary = '공공데이터 - 법정동 코드'
+  #swagger.description = "건축물 대장 조회를 위한 법정동 코드 get"
 */
 
   const address = req.query.address;
@@ -18,7 +18,8 @@ router.get("/address_code", async (req, res) => {
   const response = await axios.get(
     `https://api.vworld.kr/req/search?service=search&request=search&version=2.0&crs=EPSG:900913&size=10&page=1&query=${address}&type=address&category=road&format=json&errorformat=json&key=ACD06AF5-4717-3696-8A0F-13A93EEC7187`
   );
-  console.log("공공데이터 API - address code:  ", response?.data ?? "");
+  console.log("공공데이터 API 데이터(법정동 코드) Sent");
+  console.log(`검색 주소: ${address}`);
 
   res.send(response?.data ?? "");
 });
@@ -27,7 +28,7 @@ router.get("/building_info", async (req, res) => {
   /*
   #swagger.tags = ['공공데이터']
   #swagger.summary = '공공데이터 - 건축물대장'
-  #swagger.description = ""
+  #swagger.description = "해당 법정동 코드로 건축물 대장 정보 조회"
 */
 
   const sigunguCd = req.query.sigunguCd;
@@ -38,7 +39,7 @@ router.get("/building_info", async (req, res) => {
   const response = await axios.get(
     `https://apis.data.go.kr/1613000/BldRgstService_v2/getBrTitleInfo?sigunguCd=${sigunguCd}&bjdongCd=${bjdongCd}&bun=${bun}&ji=${ji}&ServiceKey=${process.env.PUB_DATA_SERVICE_KEY}`
   );
-  console.log("공공데이터 API - building info: ", response?.data ?? "");
+  console.log("공공데이터 API 데이터(건축물 대장) Sent");
 
   res.send(response?.data ?? "");
 });
@@ -47,7 +48,7 @@ router.get("/area_info", async (req, res) => {
   /*
   #swagger.tags = ['공공데이터']
   #swagger.summary = '공공데이터 - 지역데이터'
-  #swagger.description = ""
+  #swagger.description = "해당 장소의 주변의 지역 데이터 조회"
 */
 
   const area = req.query.area;
@@ -55,8 +56,8 @@ router.get("/area_info", async (req, res) => {
   const response = await axios.get(
     `http://openapi.seoul.go.kr:8088/4c5458685467776c37364e7a734d58/xml/citydata_ppltn/1/5/${area}`
   );
-  console.log("공공데이터 API - area info: ", response?.data ?? "");
-  console.log("공공데이터 API - area info: ", response ?? "");
+  console.log("공공데이터 API 데이터(지역 데이터) Sent");
+  console.log("검색 주소: ", area);
 
   res.send(response?.data ?? "");
 });
